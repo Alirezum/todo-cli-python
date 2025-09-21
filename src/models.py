f3ff06a1ad01849485505f3579178f928c5007a7
alireza <alirezueffati@gmail.com>
2025-09-21 12:43:33 +0330

    change strueture project

--- src/models.py
+++ src/models.py
@@ -8,9 +8,16 @@ class Task:
     title: str
     description: str = ""
     due_date: datetime | None = None
+    create_at: datetime | None = datetime.now()
     completed: bool = False
 
     def __str__(self):
         status = f"Done" if self.completed else "Pending"
         due = self.due_date.isoformat() if self.due_date else "No due"
         return f"{[self.id]} | {self.title} - {status} - ( due : {due})"
+
+
+@dataclass
+class Category:
+    id: int | None = None
+    title: str = "Other"
